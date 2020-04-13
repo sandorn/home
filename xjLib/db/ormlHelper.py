8aa7c76bd04bcee3ca617a89c74e0f0ed8093f1e
sandorn <2661767859@qq.com>
2020-04-13 18:14:31 +0800

    dbstore

--- xjLib/db/ormlHelper.py
+++ xjLib/db/ormlHelper.py
@@ -9,19 +9,28 @@
 @License: (C)Copyright 2009-2020, NewSea
 @Date: 2020-03-25 10:13:07
 @LastEditors: Even.Sand
-@LastEditTime: 2020-03-25 16:31:10
+@LastEditTime: 2020-04-05 01:43:54
 '''
 
-
 import datetime
-from sqlalchemy import Column, Integer, DateTime, Numeric, create_engine, VARCHAR
+
+from sqlalchemy import (VARCHAR, Column, DateTime, Integer, Numeric,
+                        create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dbStore import db_conf
+
+from xjLib.db.dbRouter import db_conf
 
 
 class ISqlHelper(object):
-    params = {}
+    params = {
+        'ip': None,
+        'port': None,
+        'types': None,
+        'protocol': None,
+        'country': None,
+        'area': None
+    }
 
     def init_db(self):
         raise NotImplemented
@@ -59,135 +68,146 @@ class Proxy(BaseModel):
     score = Column(Integer, nullable=False, default=20)
 
 
-class SqlHelper(ISqlHelper):
-    params = {}
+def createClass(cls):
 
-    def __init__(self):
-        if 'sqlite' in DB_CONFIG['DB_CONNECT_STRING']:
-            connect_args = {'check_same_thread': False}
-            self.engine = create_engine(
-                DB_CONFIG['DB_CONNECT_STRING'], echo=False, connect_args=connect_args
-            )
-        else:
-            self.engine = create_engine(
-                DB_CONFIG['DB_CONNECT_STRING'], echo=False
-            )
-        DB_Session = sessionmaker(bind=self.engine)
-        self.session = DB_Session()
+    class SqlHelper(ISqlHelper):
+        params = {
+            'ip': Proxy.ip,
+            'port': Proxy.port,
+            'types': Proxy.types,
+            'protocol': Proxy.protocol,
+            'country': Proxy.country,
+            'area': Proxy.area,
+            'score': Proxy.score
+        }
 
-    '''
-    对with的处理:所求值的对象必须有一个__enter__()方法，一个__exit__()方法。
-    跟with后面的语句被求值后，返回对象的__enter__()方法被调用，这个方法的返回值将被赋值给as后面的变量。当with后面的代码块全部被执行完之后，将调用前面返回对象的__exit__()方法。
-    '''
+        def __init__(self):
+            if 'sqlite' in DB_CONFIG['DB_CONNECT_STRING']:
+                connect_args = {'check_same_thread': False}
+                self.engine = create_engine(
+                    DB_CONFIG['DB_CONNECT_STRING'],
+                    echo=False,
+                    connect_args=connect_args)
+            else:
+                self.engine = create_engine(
+                    DB_CONFIG['DB_CONNECT_STRING'], echo=False)
+            DB_Session = sessionmaker(bind=self.engine)
+            self.session = DB_Session()
 
-    def __enter__(self):
-        print(self.dbName, self.odbc, "In __enter__()")
-        return self
+        def init_db(self):
+            BaseModel.metadata.create_all(self.engine)
 
-    def __exit__(self):
-        self.__del__()
-        print(self.dbName, self.odbc, "In __exit__()")
+        def drop_db(self):
+            BaseModel.metadata.drop_all(self.engine)
 
-    def __del__(self):
-        print(self.dbName, self.odbc, "In __del__()")
-        if hasattr(self, 'cur') and (self.odbc != 'connector'):
-            self.cur.close()
-        if hasattr(self, 'conn'):
-            self.conn.close()
-
-    def __str__(self):
-        """返回一个对象的描述信息"""
-        return f'sqlalchemy orm数据库对象，<dbName：[{self.dbName}] , odbc：[{self.odbc}]>\n可选驱动：[mysql.connector]；[pymysql]；[mysqlclient]；\n默认驱动[mysqlclient：MySQLdb]。'
-
-    def close(self):
-        pass
-
-    def init_db(self):
-        BaseModel.metadata.create_all(self.engine)
-
-    def drop_db(self):
-        BaseModel.metadata.drop_all(self.engine)
-
-    def insert(self, dict):
-        self.session.add(**dict)
-        self.session.commit()
-
-    def delete(self, conditions=None):
-        if conditions:
-            conditon_list = []
-            for key in list(conditions.keys()):
-                if self.params.get(key, None):
-                    conditon_list.append(self.params.get(key) == conditions.get(key))
-            conditions = conditon_list
-            query = self.session.query(Proxy)
-            for condition in conditions:
-                query = query.filter(condition)
-            deleteNum = query.delete()
+        def insert(self, value):
+            proxy = Proxy(
+                ip=value['ip'],
+                port=value['port'],
+                types=value['types'],
+                protocol=value['protocol'],
+                country=value['country'],
+                area=value['area'],
+                speed=value['speed'])
+            self.session.add(proxy)
             self.session.commit()
-        else:
-            deleteNum = 0
-        return ('deleteNum', deleteNum)
 
-    def update(self, conditions=None, value=None):
-        '''
-        conditions的格式是个字典。类似self.params
-        param value:也是个字典：{'ip':192.168.0.1}
-        return:字典,'updateNum': update成功数量
-        '''
-        if conditions and value:
-            conditon_list = []
-            for key in list(conditions.keys()):
-                if self.params.get(key, None):
-                    conditon_list.append(self.params.get(key) == conditions.get(key))
-            conditions = conditon_list
-            query = self.session.query(Proxy)
-            for condition in conditions:
-                query = query.filter(condition)
-            updatevalue = {}
-            for key in list(value.keys()):
-                if self.params.get(key, None):
-                    updatevalue[self.params.get(key, None)] = value.get(key)
-            updateNum = query.update(updatevalue)
-            self.session.commit()
-        else:
-            updateNum = 0
-        return {'updateNum': updateNum}
+        def delete(self, conditions=None):
+            if conditions:
+                conditon_list = []
+                for key in list(conditions.keys()):
+                    if self.params.get(key, None):
+                        conditon_list.append(
+                            self.params.get(key) == conditions.get(key))
+                conditions = conditon_list
+                query = self.session.query(Proxy)
+                for condition in conditions:
+                    query = query.filter(condition)
+                deleteNum = query.delete()
+                self.session.commit()
+            else:
+                deleteNum = 0
+            return ('deleteNum', deleteNum)
 
-    def select(self, count=None, conditions=None):
-        '''
-        conditions的格式是个字典。类似self.params
-        :param count:
-        :param conditions:
-        :return:
-        '''
-        if conditions:
-            conditon_list = []
-            for key in list(conditions.keys()):
-                if self.params.get(key, None):
-                    conditon_list.append(self.params.get(key) == conditions.get(key))
-            conditions = conditon_list
-        else:
-            conditions = []
+        def update(self, conditions=None, value=None):
+            '''
+            conditions的格式是个字典。类似self.params
+            :param conditions:
+            :param value:也是个字典：{'ip':192.168.0.1}
+            :return:
+            '''
+            if conditions and value:
+                conditon_list = []
+                for key in list(conditions.keys()):
+                    if self.params.get(key, None):
+                        conditon_list.append(
+                            self.params.get(key) == conditions.get(key))
+                conditions = conditon_list
+                query = self.session.query(Proxy)
+                for condition in conditions:
+                    query = query.filter(condition)
+                updatevalue = {}
+                for key in list(value.keys()):
+                    if self.params.get(key, None):
+                        updatevalue[self.params.get(key, None)] = value.get(key)
+                updateNum = query.update(updatevalue)
+                self.session.commit()
+            else:
+                updateNum = 0
+            return {'updateNum': updateNum}
 
-        query = self.session.query(Proxy.ip, Proxy.port, Proxy.score)
-        if len(conditions) > 0 and count:
-            for condition in conditions:
-                query = query.filter(condition)
-            return query.order_by(Proxy.score.desc(), Proxy.speed).limit(count).all()
-        elif count:
-            return query.order_by(Proxy.score.desc(), Proxy.speed).limit(count).all()
-        elif len(conditions) > 0:
-            for condition in conditions:
-                query = query.filter(condition)
-            return query.order_by(Proxy.score.desc(), Proxy.speed).all()
-        else:
-            return query.order_by(Proxy.score.desc(), Proxy.speed).all()
+        def select(self, count=None, conditions=None):
+            '''
+            conditions的格式是个字典。类似self.params
+            :param count:
+            :param conditions:
+            :return:
+            '''
+            if conditions:
+                conditon_list = []
+                for key in list(conditions.keys()):
+                    if self.params.get(key, None):
+                        conditon_list.append(
+                            self.params.get(key) == conditions.get(key))
+                conditions = conditon_list
+            else:
+                conditions = []
+
+            query = self.session.query(Proxy.ip, Proxy.port, Proxy.score)
+            if len(conditions) > 0 and count:
+                for condition in conditions:
+                    query = query.filter(condition)
+                return query.order_by(Proxy.score.desc(),
+                                      Proxy.speed).limit(count).all()
+            elif count:
+                return query.order_by(Proxy.score.desc(),
+                                      Proxy.speed).limit(count).all()
+            elif len(conditions) > 0:
+                for condition in conditions:
+                    query = query.filter(condition)
+                return query.order_by(Proxy.score.desc(), Proxy.speed).all()
+            else:
+                return query.order_by(Proxy.score.desc(), Proxy.speed).all()
+
+        def close(self):
+            pass
+
+    return SqlHelper
 
 
 if __name__ == '__main__':
     sqlhelper = SqlHelper()
     sqlhelper.init_db()
-    proxy = {'ip': '192.168.1.1', 'port': 80, 'type': 0, 'protocol': 0, 'country': '中国', 'area': '广州', 'speed': 11.123, 'types': ''}
+    proxy = {
+        'ip': '192.168.1.1',
+        'port': 80,
+        'type': 0,
+        'protocol': 0,
+        'country': '中国',
+        'area': '广州',
+        'speed': 11.123,
+        'types': ''
+    }
     sqlhelper.insert(proxy)
     sqlhelper.update({'ip': '192.168.1.1', 'port': 80}, {'score': 10})
     print(sqlhelper.select(1))
