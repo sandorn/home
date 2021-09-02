228658461767ef42d7e829b7d7c14fd0ced92ff2
sandorn <sandorn@live.cn>
2021-09-02 16:26:25 +0800

    1

--- xjLib/xt_Head.py
+++ xjLib/xt_Head.py
@@ -16,7 +16,8 @@ Github       : https://github.com/sandorn/home
 from fake_useragent import UserAgent
 
 MYHEAD = {
-    'User-Agent': UserAgent().random,
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    # #' User-Agent': UserAgent.random,
     'Accept': '*/*,application/*,application/json,text/*,text/html',
     'Accept-Encoding': 'gzip,compress,deflate,br',
     'Content-Encoding': 'gzip,deflate,compress',
