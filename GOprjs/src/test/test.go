b041de686842773904b94ad1be30468c1a12d65c
sandorn <sandorn@live.cn>
2022-12-17 01:04:02 +0800

    golang

---
+++ GOprjs/src/test/test.go
@@ -0,0 +1,19 @@
+/*
+ * @Description  :
+ * @Develop      : VSCode
+ * @Author       : Even.Sand
+ * @Contact      : sandorn@163.com
+ * @Date         : 2022-12-17 00:26:44
+ * @LastEditTime : 2022-12-17 00:26:45
+ * @FilePath     : /GOprjs/src/test/test.go
+ * @Github       : https://github.com/sandorn/home
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hell world\n按下回车键退出...")
+	fmt.Scanf("%s")
+	fmt.Println("结束")
+}
