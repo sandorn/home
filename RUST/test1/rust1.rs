4b79b393ef5fd0ff8e873d6c2298846048b88db7
sandorn <sandorn@live.cn>
2023-03-31 14:55:11 +0800

    rust

--- RUST/test1/rust1.rs
+++ RUST/test1/rust1.rs
@@ -1,29 +1,31 @@
-fn print_char(value: &str) {
-    println!("a is {}", value);
-}
+// fn print_char(value: &str) {
+//     println!("a is {}", value);
+// }
 
-fn main1() {
-    let x = "11112222 hello,world!";
-    print_char(x);
-}
-fn main2() {
-    let x = 5;
+// fn main1() {
+//     let x = "11112222 hello,world!";
+//     print_char(x);
+// }
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
+// fn main2() {
+//     let x = 5;
 
-    println!("x 的值为 : {}", x);
-    println!("y 的值为 : {}", y);
-}
+//     let y = {
+//         let x = 3;
+//         x + 1
+//     };
+
+//     println!("x 的值为 : {}", x);
+//     println!("y 的值为 : {}", y);
+// }
 
 fn main() {
     fn five() -> i32 {
         5
     }
-    println!("five() 的值为: {},{}", five(),add(1, 2));
+    println!("five() 的值为: {},{}", five(), add(1, 2));
 }
+
 fn add(a: i32, b: i32) -> i32 {
     return a + b;
 }
