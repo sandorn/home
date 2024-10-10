2440b0bc3f1765d2bf99f36079e068be15c8ae14
sandorn <sandorn@live.cn>
2024-10-10 13:27:56 +0800

    java

---
+++ rust_app_1/main.java
@@ -0,0 +1,16 @@
+/*
+ * @Description  : 头部注释
+ * @Develop      : VSCode
+ * @Author       : sandorn sandorn@live.cn
+ * @Date         : 2024-10-10 13:07:15
+ * @LastEditTime : 2024-10-10 13:26:50
+ * @FilePath     : /CODE/rust_app_1/main.java
+ * @Github       : https://github.com/sandorn/home
+ */
+
+//写一个打印的小例子
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
